0e7b6f67a71ac00b74713e73026e4f736f6d74ec
LUISGERARDO11 <luis34julieta@gmail.com>
2025-11-04 15:30:30 -0600

    actualizacion de librerias

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,19 @@
-// babel.config.js
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      // 🚀 CRÍTICO: Debe estar para que Expo Router funcione en el build de EAS
-      'expo-router/babel', 
-      
-      // Mantén tus otros plugins solo si son necesarios
-      ['@babel/plugin-transform-flow-strip-types'],
-      ['@babel/plugin-transform-class-properties', { loose: true }],
-      ['@babel/plugin-transform-private-methods', { loose: true }],
-      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-      ['inline-dotenv', {
-        unsafe: {
-          EXPO_OS: 'android',
-        },
-      }],
-      // Agrega el plugin de Reanimated si lo usas
-      // 'react-native-reanimated/plugin',
-    ],
-  };
+// babel.config.js - ESTA ES LA VERSIÓN CORRECTA
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    // 🚀 ESTO ES VITAL Y ESTABA FALTANDO
+    'expo-router/babel', 
+
+    // Tus otros plugins
+    ['@babel/plugin-transform-flow-strip-types'],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    ['inline-dotenv', {
+      unsafe: {
+        EXPO_OS: 'android',
+      },
+    }],
+  ],
 };
