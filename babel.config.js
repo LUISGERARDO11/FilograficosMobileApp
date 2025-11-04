51576b7e4a9ce38385f03e348cd90418fda20ab2
LUISGERARDO11 <luis34julieta@gmail.com>
2025-11-04 17:53:09 -0600

    Simplificacion del babel asi como actualizacion de versiones de librerias

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,6 @@
-// babel.config.js - CORRECCIÓN DEFINITIVA
-module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: [
-    'expo-router/babel',
-    
-    ['@babel/plugin-transform-flow-strip-types'],
-    ['@babel/plugin-transform-class-properties', { loose: true }],
-    ['@babel/plugin-transform-private-methods', { loose: true }],
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-    ['inline-dotenv', {
-      unsafe: {
-        EXPO_OS: 'android',
-      },
-    }],
-  ],
+﻿module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
