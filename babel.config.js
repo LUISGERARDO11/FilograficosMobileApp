1d7adccb28f077d5155762509e55d47496769a78
LUISGERARDO11 <luis34julieta@gmail.com>
2025-11-04 16:53:25 -0600

    expo router añadido en babel

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   presets: ['babel-preset-expo'],
   plugins: [
-    // La funcionalidad de expo-router/babel ya está en presets: ['babel-preset-expo']
+    'expo-router/babel',
     
     ['@babel/plugin-transform-flow-strip-types'],
     ['@babel/plugin-transform-class-properties', { loose: true }],
