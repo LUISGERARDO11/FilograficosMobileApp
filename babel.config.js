898c3dda1d4a772931ba5c164ebbc18a10470c60
LUISGERARDO11 <luis34julieta@gmail.com>
2025-11-04 14:54:55 -0600

    actualizacion de librerias

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,24 @@
 // babel.config.js
-module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: [
-    ['@babel/plugin-transform-flow-strip-types'],
-    ['@babel/plugin-transform-class-properties', { loose: true }],
-    ['@babel/plugin-transform-private-methods', { loose: true }],
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-    // Add environment variable inline plugin
-    ['inline-dotenv', {
-      unsafe: {
-        EXPO_OS: 'android',
-      },
-    }],
-  ],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // 🚀 CRÍTICO: Debe estar para que Expo Router funcione en el build de EAS
+      'expo-router/babel', 
+      
+      // Mantén tus otros plugins solo si son necesarios
+      ['@babel/plugin-transform-flow-strip-types'],
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+      ['inline-dotenv', {
+        unsafe: {
+          EXPO_OS: 'android',
+        },
+      }],
+      // Agrega el plugin de Reanimated si lo usas
+      // 'react-native-reanimated/plugin',
+    ],
+  };
 };
